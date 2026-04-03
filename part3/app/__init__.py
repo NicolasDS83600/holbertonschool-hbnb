3afca0b80755cd60345cda467d057883500dc33f
NicolasDS83600 <nico836001@outlook.fr>
2026-04-03 14:52:48 +0200

    feat: add data in the db and connexion back-end/front-end

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
  
 bcrypt = Bcrypt()
 jwt = JWTManager()
@@ -20,6 +21,8 @@ def create_app(config_class):
     jwt.init_app(app)
     db.init_app(app)
 
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+
     from app.api.v1 import namespaces as v1_namespaces
 
     api = Api(
@@ -32,5 +35,5 @@ def create_app(config_class):
  
     for ns in v1_namespaces:
         api.add_namespace(ns, path=f'/api/v1/{ns.name}')
- 
+
     return app
